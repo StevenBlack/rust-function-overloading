1dc87749f019292938c2d1e13f6809836a09b47a
Steven Black <steveb@stevenblack.com>
2023-03-12 18:50:09 -0400

    First commit.

---
+++ readme.md
@@ -0,0 +1,5 @@
+# rust-function-overloading
+
+This is a simple example of function overloading in Rust.
+
+See: https://www.reddit.com/r/rust/comments/11p88n6/comment/jbwmu3v/?utm_source=share&utm_medium=web2x&context=3
---
+++ src/main.rs
@@ -0,0 +1,41 @@
+
+#[derive(Debug)]
+struct Dog {
+    name: String,
+}
+
+impl From<String> for Dog {
+    fn from(value: String) -> Dog {
+        Dog {name: value}
+    }
+}
+
+impl From<i32> for Dog {
+    fn from(value: i32) -> Dog {
+        Dog {name: String::from(value.to_string())}
+    }
+}
+
+trait Bark<T> {
+    fn bark(&self, s: T);
+}
+
+impl Bark<String> for Dog {
+    fn bark(&self, s: String) {
+        let _ = s;
+        println!("{} bark on string type", self.name);
+    }
+}
+
+impl Bark<i32> for Dog {
+    fn bark(&self, s: i32) {
+        let _ = s;
+        println!("{} bark on int type", self.name);
+    }
+}
+
+fn main() {
+    let d = Dog::from("toby".to_string());
+    d.bark("string".to_string());
+    d.bark(4);
+}
